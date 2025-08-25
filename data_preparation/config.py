71045211f87685062c05e4ae505a44960c9f9e89
quinbez <bezawitt9@gmail.com>
2025-08-25 10:22:56 +0300

    add configuration for data preparaion

---
+++ data_preparation/config.py
@@ -0,0 +1,12 @@
+import os
+class Config:
+    VOCAB_SIZE = None
+    PAD_ID=None
+    EOS_ID=None
+    MAX_LENGTH = 128
+    DATASET_NAME = "opwb" 
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
+    DATA_DIR = os.path.join(BASE_DIR, "datasets") 
+    TOKENIZER_DIR = os.path.join(BASE_DIR, "tokenizer", "outputs")  
+    BATCH_SIZE = 32
+    num_workers = 8
