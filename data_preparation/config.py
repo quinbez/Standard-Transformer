ae02c47b58ef07331c0b8e841b4b66c978c6d506
Bethlehem <93065948+Betty987@users.noreply.github.com>
2025-08-25 15:31:12 +0300

    changed dataset to ptb

--- data_preparation/config.py
+++ data_preparation/config.py
@@ -4,7 +4,7 @@ class Config:
     PAD_ID=None
     EOS_ID=None
     MAX_LENGTH = 128
-    DATASET_NAME = "opwb" 
+    DATASET_NAME = "ptb" 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
     DATA_DIR = os.path.join(BASE_DIR, "datasets") 
     TOKENIZER_DIR = os.path.join(BASE_DIR, "tokenizer", "outputs")  
