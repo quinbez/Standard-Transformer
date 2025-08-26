14a4f0edacc047b17178c636571f9e951bd72455
Bethlehem <93065948+Betty987@users.noreply.github.com>
2025-08-26 12:29:19 +0300

    change the learning rate to 1e-4

--- model_architecture/config.py
+++ model_architecture/config.py
@@ -6,11 +6,11 @@ class GPTConfig:
     pad_token_id = None
     eos_token_id = None
     block_size = 256
-    learning_rate = 1e-5
+    learning_rate = 1e-4
     n_embd = 64
     n_head = 2
     n_layer = 2
     dropout = 0.1
-    max_epochs = 10
+    max_epochs = 1
     max_new_tokens = 50
     temperature = 1.0
